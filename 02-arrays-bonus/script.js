b5f619535bc374d50ea0cd490386acc1d14fc8e5
GiacomoGaudino <giacomo.gaudino@yahoo.it>
2025-07-10 23:51:35 +0200

    exercise bonus 5 done

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -46,4 +46,12 @@ for (i = 0; i < teachers.length; i++) {
 console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let teachersString = "";
+for (i = 0; i < teachers.length; i++) {
+  const this_teacher = teachers[i];
+  teachersString += teachers[i];
+  if (i < teachers.length - 1) {
+    teachersString += ',';
+  }
+}
+console.log(teachersString);
